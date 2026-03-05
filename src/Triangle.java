c65be63155261da7f3f2a57f49a67b7175a72d65
Milana Rose <milanar292@gmail.com>
2026-03-05 14:16:45 +0530

    added

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,14 @@
 public class Triangle {
     //Add your programs here
+    public static void main(String args[]){
+        for(int i=1; i<=5;i+=2){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+                if(j != i){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
